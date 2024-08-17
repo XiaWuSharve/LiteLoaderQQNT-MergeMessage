3752855e62ec22b66b88e2954694a95ddb3c42f2
XiaWuSharve <sharve@foxmail.com>
2024-08-17 10:49:32 +0800

    rm symbolic link

--- src/preload.js
+++
@@ -1,8 +0,0 @@
-// Electron 主进程 与 渲染进程 交互的桥梁
-const { contextBridge, ipcRenderer } = require("electron");
-
-
-// 在window对象下导出只读对象
-contextBridge.exposeInMainWorld("plugins", {
-    updateMessage: (newMessage) => ipcRenderer.send('update-message', newMessage)
-});
