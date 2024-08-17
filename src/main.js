a59b85a7c34897e4eb8d9be255fa6068424994e3
XiaWuSharve <sharve@foxmail.com>
2024-08-17 14:44:12 +0800

    fix: 读写文件同步问题

--- src/main.js
+++ src/main.js
@@ -2,22 +2,30 @@
 const { ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('node:path');
+let mutex = Promise.resolve();
 
-ipcMain.on('update-message', (e, newMessage) => {
+ipcMain.on('update-message', async (e, newMessage) => {
     // 等别人或自己设置插件ui
     // const displayMessageCount = 10;
-    const filename = 'MergeMessage.txt';
-    const filePath = path.join(__dirname, filename);
-    const messageLine = newMessage.replace(/\n/g, '。')
-    fs.readFile(filePath, { encoding: 'utf-16le' }, (err, data) => {
-        if(err) throw err;
-        const messages = data.toString('utf16le').split('\n');
-        messages.splice(0, 1);
-        messages.push(messageLine);
-        fs.writeFile(filePath, '\ufeff' + messages.join('\n'), { encoding: 'utf-16le' }, err => {
-            throw err;
-        });
-    })
+    await mutex;
+    mutex = (async () => {
+        const filename = 'MergeMessage.txt';
+        const filePath = path.join(__dirname, filename);
+        const messageLine = newMessage.replace(/\n/g, '。')
+        await new Promise(resolve => fs.readFile(filePath, { encoding: 'utf-16le' }, (err, data) => {
+            if(err) throw err;
+            const messages = data.toString('utf16le').split('\n');
+            messages.splice(0, 1);
+            messages.push(messageLine);
+            fs.writeFile(filePath, '\ufeff' + messages.join('\n'), { encoding: 'utf-16le' }, err => {
+                if(err)
+                    throw err;
+                else
+                    resolve();
+            });
+        }));
+        return Promise.resolve();
+    })();
 })
 
 // 创建窗口时触发
